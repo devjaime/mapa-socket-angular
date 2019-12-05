0e092d3c851e9f97ff49aa3f001a0022ba84f798
Jaime Hernandez <hernandez.hs@gmail.com>
2019-12-05 20:23:19 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+// socket
+import { SocketIoModule } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    SocketIoModule.forRoot(environment.socketConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
